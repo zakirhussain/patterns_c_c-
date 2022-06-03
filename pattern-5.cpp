938837c142c4ebd019d41adfcbefd319822d5858
ZAKIR HUSSAIN <pro.zakir24@gmail.com>
2022-06-03 11:54:34 +0530

    Add files via upload

---
+++ pattern-5.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+
+/*
+1 2 3 4 
+5 6 7 8
+9 10 11 12
+13 14 15 16
+*/
+
+int main()
+{
+    int row = 4, col = 4;
+    int i=1;
+    int cnt=1;
+    
+    while(i<=row){
+        int j = 1;
+        while(j<=col){
+            std::cout << cnt<< " " ;
+            cnt++;
+            j++;
+        }
+        std::cout<<"\n";
+        i++;
+    }
+    return 0;
+}
